33e902013dbb812888aa991deb26007028d3cb76
irenaproj <irena.shemesh@gmail.com>
2015-08-16 20:35:21 +0000

    Refactor Ruby code

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -8,69 +8,70 @@ class StaticPagesController < ApplicationController
     # dataType - specifies the data to retrieve
     def initialize(dataType)  
       # Instance variables  
-      @dataType = dataType  
+      @dataType  = dataType  
+      @@username = "72db99d0-05dc-0133-cefe-22000a93862b"
+      @@password = "_cIOpimIoDi3RIviWteOTA"
+      @@url      = "http://api-impac-uat.maestrano.io/api/v1/get_widget"
     end  
     
-    # Read raw data per provided type
+    # Read raw data per type provided 
     def getMnoData  
-      #curl -g -k -u 72db99d0-05dc-0133-cefe-22000a93862b:_cIOpimIoDi3RIviWteOTA -H "Accept: application/json" "https://api-impac-uat.maestrano.io/api/v1/get_widget?engine=hr/employees_list&metadata[organization_ids][]=org-fbte"
-      
       if @dataType == 'employees_list'
-        params = MultiJson.load('{"engine":"hr/employees_list", "metadata[organization_ids][]": "org-fbte"}')
+        params = MultiJson.load('{"engine":"hr/employees_list", 
+                                  "metadata[organization_ids][]": "org-fbte"}')
       elsif @dataType == 'employee_details'
-        params = MultiJson.load('{"engine":"hr/employee_details", "metadata[organization_ids][]": "org-fbte"}')
+        params = MultiJson.load('{"engine":"hr/employee_details", 
+                                  "metadata[organization_ids][]": "org-fbte"}')
       elsif @dataType == 'invoices'
-        params = MultiJson.load('{"engine":"invoices/list", "metadata[organization_ids][]": "org-fbte", "metadata[entity]": "customers|suppliers"}')
+        params = MultiJson.load('{"engine":"invoices/list", 
+                                "metadata[organization_ids][]": "org-fbte", 
+                                "metadata[entity]": "customers|suppliers"}')
       else
          raise StandardError
       end      
       
-      auth = {username: "72db99d0-05dc-0133-cefe-22000a93862b", password: "_cIOpimIoDi3RIviWteOTA"}
+      auth = {username: @@username, password: @@password}
       options = {query: params, basic_auth: auth}
       
-      @content = HTTParty.get("http://api-impac-uat.maestrano.io/api/v1/get_widget", options)
+      @content = HTTParty.get(@@url, options)
     end  
   end  
-
-  # Prepare data to be displayed on "Employees location" widget - acquire raw data and process as following:
-  # data received from hr/employees_list and hr/employee_details is almost identical, locations can be 
-  # deducted based on employees_list only. In addition, "work_locations" key of some employees is empty and,
-  # where exists - it specifies the location via ID only, thus, for the sake of the exercise, widget will 
-  # be based on address field instead.
-  def employeeLocationWidgetPrepare()
-    locations = Hash.new
-    
-    employeesList = (RawData.new("employee_details").getMnoData())['content']['employees']
-    employeesList.each do | employee |
-      # extract the city/country information from the 'address' field
-      address = employee['address']
-      location = employee['address'].gsub(/\ \d+/, "")
-      location =location.split(/\s*[,;]\s*/x).from(-2).join(', ')
-      
-      # Update quantity in Hash
-      occurrence = locations.fetch(location, nil)
-      
-      if !occurrence
-         locations[location] = Array.new
-      end    
-      
-      locations[location].push(address)
-      
-      #puts locations
-    end
-
-    return locations
-  end  
   
-  # Prepare data to be displayed on "Employees location" widget - acquire raw data and process as following:
-  # data received from hr/employees_list and hr/employee_details is almost identical, locations can be 
-  # deducted based on employees_list only. In addition, "work_locations" key of some employees is empty and,
-  # where exists - it specifies the location via ID only, thus, for the sake of the exercise, widget will 
-  # be based on address field instead.
-  def employeeLocationWidgetPrepare()
+  # Add common map details
+  def finalizeMap(widgetType, data)
+    
+    # Add map config: center and zoom
+    if widgetType == 'employee_details'
+      data["center"] = { 'lat'=> 35.864716, 'lng'=> 2.349014, 'zoom'=> 1  }
+    elsif widgetType == 'invoices'
+      data["center"] = { 'lat'=> -23.6974800, 'lng'=> 133.8836200, 'zoom'=> 3 }
+    else
+      raise StandardError
+    end
+    
+    # Add baselayers
+    data['layers']['baselayers'] = {
+      'mapbox_light' => {
+        'name' => 'Mapbox Light',
+        'url' => 'http://api.tiles.mapbox.com/v4/{mapid}/{z}/{x}/{y}.png?access_token={apikey}',
+        'type' => 'xyz',
+        'layerOptions' => {
+          # Optional for the free service
+          'apikey' => 'pk.eyJ1IjoiYnVmYW51dm9scyIsImEiOiJLSURpX0pnIn0.2_9NrLz1U9bpwMQBhVk97Q',
+          'mapid' => 'bufanuvols.lia22g09'
+        }
+      }
+    }
+    
+    return data
+  end
+  
+  # Group all employees by city/country - for marker clustering
+  def employeeLocationsByCity()
     locations = Hash.new
     
     employeesList = (RawData.new("employee_details").getMnoData())['content']['employees']
+    
     employeesList.each do | employee |
       # extract the city/country information from the 'address' field
       address = employee['address']
@@ -92,6 +93,56 @@ class StaticPagesController < ApplicationController
     return locations
   end  
   
+  # Prepare data to be displayed on "Employees location" widget - acquire raw data and process as following:
+  # data received from hr/employees_list and hr/employee_details is almost identical, locations can be 
+  # deducted based on employees_list only. In addition, "work_locations" key of some employees is empty and,
+  # where exists - it specifies the location via ID only, thus, for the sake of the exercise, widget will 
+  # be based on address field instead.
+  def employeeLocationWidgetPrepare()
+    workLocationsData = Hash.new
+    markers = Hash.new
+    count = 0
+
+    # Add layers: baselayers and overlays
+    workLocationsData['layers'] = Hash.new
+    
+    # Add overlays
+    workLocationsData['layers']['overlays'] = Hash.new
+    
+    locations = employeeLocationsByCity()
+
+    locations.each do | key, addrArray |
+      
+      # Build each marker and add to the list
+      addrArray.each { |addr|
+        marker = Hash.new
+        count += 1
+
+        # Build marker
+        result = Geocoder.search(addr)
+
+        marker['layer'] = key
+        marker['lat'] = result[0].latitude
+        marker['lng'] = result[0].longitude
+        
+        # Add to the list
+        markers["addr" + "#{count}"] = marker
+      }
+      
+      # Integrate markers
+      workLocationsData['markers'] = Hash.new
+      workLocationsData['markers'] = markers
+      
+      # Build overlays - allow removing data from the map
+      workLocationsData['layers']['overlays'][key] = Hash.new
+      workLocationsData['layers']['overlays'][key]['name'] = key
+      workLocationsData['layers']['overlays'][key]['type'] = 'markercluster'
+      workLocationsData['layers']['overlays'][key]['visible'] = true
+    end
+    
+    return finalizeMap('employee_details', workLocationsData)
+  end
+  
   # Prepare data to be displayed on "Sales flow" widget - acquire raw data and process as following:
   # Retrieve data via Impac! API, where "address" field contains enough data to allow geolocation,
   # add address and the invoice data to the records 
@@ -104,11 +155,9 @@ class StaticPagesController < ApplicationController
 
     markers = Hash.new
     invoicesData = Hash.new
-    minAmount = 1152921504606846976 # 2^60
+    minAmount = 1152921504606846976 # 2^60, should be enough
     maxAmount = -1
     recNum = 0
-    
-    invoicesData["center"] = { 'lat'=> -23.6974800, 'lng'=> 133.8836200, 'zoom'=> 3 }
     
     invoicesList = (RawData.new("invoices").getMnoData())['content']['entities']
     
@@ -153,113 +202,28 @@ class StaticPagesController < ApplicationController
       end    
     }
 
-    puts "33333333"
-    puts minAmount
-    puts maxAmount
-      
     # Opacity range [0.4, 1], invoices range [minAmount, maxAmount],
     # calculate relative opacity for each marker 
-    if maxAmount > minAmount
-      markers.each do | record, value |
-        markerTotal = value['total']
-        puts markerTotal
-        value['opacity'] = min_opacity + ((( markerTotal - minAmount) / ( maxAmount - minAmount )) * opacity_range)
-        value.delete 'total'
-        puts value
+    markers.each do | record, value |
+      if maxAmount > minAmount
+        value['opacity'] = min_opacity + ((( value['total'] - minAmount) / ( maxAmount - minAmount )) * opacity_range)
       end
+
+      # Field no longer needed
+      value.delete 'total'
     end
     
-    puts "4444444"
-    puts markers
-    
-    # Add baselayers
+    # Add baselayers - will be filled later
     invoicesData['layers'] = Hash.new
     
-    # Add baselayers
-    invoicesData['layers']['baselayers'] = {
-      'mapbox_light' => {
-        'name' => 'Mapbox Light',
-        'url' => 'http://api.tiles.mapbox.com/v4/{mapid}/{z}/{x}/{y}.png?access_token={apikey}',
-        'type' => 'xyz',
-        'layerOptions' => {
-          # Optional for the free service
-          'apikey' => 'pk.eyJ1IjoiYnVmYW51dm9scyIsImEiOiJLSURpX0pnIn0.2_9NrLz1U9bpwMQBhVk97Q',
-          'mapid' => 'bufanuvols.lia22g09'
-        }
-      }
-    }
-    
+    # Integrate markers into the map structure
     invoicesData['markers'] = markers
     
-    return invoicesData
+    return finalizeMap('invoices', invoicesData)
   end  
   
-  # Build object as expected by the map plugin
-  def prepareMapMarkers(locations)
-    workLocationsData = Hash.new
-    markers = Hash.new
-    count = 0
-    
-    # Add map config
-    workLocationsData["center"] = { 'lat'=> 35.864716, 'lng'=> 2.349014, 'zoom'=> 1  }
-
-    # Add layers: baselayers and overlays
-    workLocationsData['layers'] = Hash.new
-    
-    # Add baselayers
-    workLocationsData['layers']['baselayers'] = {
-      'mapbox_light' => {
-        'name' => 'Mapbox Light',
-        'url' => 'http://api.tiles.mapbox.com/v4/{mapid}/{z}/{x}/{y}.png?access_token={apikey}',
-        'type' => 'xyz',
-        'layerOptions' => {
-          # Optional for the free service
-          'apikey' => 'pk.eyJ1IjoiYnVmYW51dm9scyIsImEiOiJLSURpX0pnIn0.2_9NrLz1U9bpwMQBhVk97Q',
-          'mapid' => 'bufanuvols.lia22g09'
-        }
-      }
-    }
-    
-    # Add overlays
-    workLocationsData['layers']['overlays'] = Hash.new
-
-    locations.each do | key, addrArray |
-      
-      # Build each marker and add to the list
-      addrArray.each { |addr|
-        marker = Hash.new
-        count += 1
-        puts addr
-        
-        # Build marker
-        result = Geocoder.search(addr)
-
-        marker['layer'] = key
-        marker['lat'] = result[0].latitude
-        marker['lng'] = result[0].longitude
-        
-        # Add to the list
-        markers["addr" + "#{count}"] = marker
-      }
-      
-      # Integrate markers
-      workLocationsData['markers'] = Hash.new
-      workLocationsData['markers'] = markers
-      
-      # Build overlays - allow removing data from the map
-      workLocationsData['layers']['overlays'][key] = Hash.new
-      workLocationsData['layers']['overlays'][key]['name'] = key
-      workLocationsData['layers']['overlays'][key]['type'] = 'markercluster'
-      workLocationsData['layers']['overlays'][key]['visible'] = true
-    end
-    
-    return workLocationsData
-  end
-  
   def home
-    locationsData = employeeLocationWidgetPrepare()
-    @employeeLocationsData = prepareMapMarkers(locationsData)
-    
+    @employeeLocationsData = employeeLocationWidgetPrepare()
     @invoices = salesFlowWidgetPrepare()
   end
 
